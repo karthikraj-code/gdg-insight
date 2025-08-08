e8cd1dc51333ad2e6655291c3eec1824e2a9fdbc
KarthikRaju <aradhulakarthik@gmail.com>
2025-08-08 13:41:38 +0530

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -263,7 +263,7 @@ const Index = () => {
               className="h-8 w-auto object-contain"
             />
             <p className="text-muted-foreground">
-              Made with ❤️ by the Google Developers Group Kare(GDG) Team
+              Made with ❤️ by the Google Developers Group KARE(GDG) Team
             </p>
           </div>
           
